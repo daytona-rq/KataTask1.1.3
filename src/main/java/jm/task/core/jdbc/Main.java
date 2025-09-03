2f88d14ed417d61d74e255d6dbcc112848c47811
daytona <onton235@gmail.com>
2025-09-03 11:18:00 +0400

    v3 clean

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -4,10 +4,6 @@ import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
-
 public class Main {
     public static void main(String[] args) {
         try {
